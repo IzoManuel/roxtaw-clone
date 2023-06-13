c2ab4c77ed1d82558b43aa955f91868ceea2d8f2
IzoIbrahim <israel@brrng.com>
2023-06-13 11:49:09 +0300

    Fix splash logo position

--- src/layouts/base-layout.vue
+++ src/layouts/base-layout.vue
@@ -5,19 +5,19 @@ import { ref, onMounted, computed } from "vue";
 import RoxtawLogo from "../assets/svgs/roxtaw-logo.vue";
 
 /**REACTIVE STATES */
-const splashOpacity = ref(1)
-const splashLogoFillOpacity = ref('rgb(0 0 0 / 0)');
+const splashOpacity = ref(1);
+const splashLogoFillOpacity = ref("rgb(0 0 0 / 0)");
 
 /**COMPUTED PROPERTIES*/
 const splashStyle = computed(() => {
   return {
-    opacity:`${splashOpacity.value}`,
+    opacity: `${splashOpacity.value}`,
     transition: `opacity 0.5s`,
   };
 });
 const splashLogoStyle = computed(() => {
   return {
-    fill: 'rgb(0 0 0 / 1)',
+    fill: "rgb(0 0 0 / 1)",
     transition: `fill 5s`,
   };
 });
@@ -39,11 +39,13 @@ onMounted(() => {
 <template>
   <div
     id="splash-screen"
-    class="fixed top-0 right-0 bottom-0 left-0 w-full overflow-hidden pointer-events-none bg-[#f7f8f2] z-50 max-h-full"
+    class="fixed flex items-center top-0 right-0 bottom-0 left-0 w-full overflow-hidden pointer-events-none bg-[#f7f8f2] z-50 max-h-full"
     :style="splashStyle"
   >
-<roxtaw-logo class="splashLogo w-[400px] mx-auto stroke-black stroke-1"></roxtaw-logo>
-</div>
+    <div id="brand-logo" class="relative w-[200px] mx-auto align-middle">
+      <roxtaw-logo class="splashLogo stroke-black stroke-1"></roxtaw-logo>
+    </div>
+  </div>
   <main-header></main-header>
 
   <div id="main-content" class="relative">
@@ -59,10 +61,9 @@ onMounted(() => {
   animation: splashLogoFadeIn 2s ease-out 1s forwards;
 }
 
-@keyframes splashLogoFadeIn{
+@keyframes splashLogoFadeIn {
   to {
-    fill: rgb(0 0 0 / 1)
+    fill: rgb(0 0 0 / 1);
   }
 }
-
 </style>
